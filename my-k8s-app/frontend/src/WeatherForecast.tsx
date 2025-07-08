dc18a775c4af84c2a6b2c1fc7654cc5ca92ca8b2
Sarah Ashri <sarah.ashri@octopus.com>
2025-07-08 16:49:10 +1000

    Updated my-k8s-app frontend to (try to) call the backend
    Note: the call doesn't work at this point because of Cross-Origin Resource Sharing (CORS). Since I want to containerize the apps anyeay, I'll containerise the apps first and then fix them retry

---
+++ my-k8s-app/frontend/src/WeatherForecast.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+import {WeatherForecastResource} from "./types/WeatherForcastResource";
+
+
+interface State {
+  forecast: WeatherForecastResource[];
+}
+
+export class WeatherForecast extends React.Component<{}, State> {
+    constructor(props: {}) {
+        super(props);
+        this.state = { forecast: [] };
+    }
+    
+    async componentDidMount() {
+        const response = await fetch('http://localhost:5209/WeatherForecast');
+        if (response.ok) {
+        const data: WeatherForecastResource[] = await response.json();
+        this.setState({ forecast: data });
+        } else {
+        console.error('Failed to fetch weather forecast');
+        }
+    }
+    
+    render() {
+        return (
+        <div className="App">
+            <header className="App-header">
+            <h1>Weather Forecast</h1>
+            <ul>
+                {this.state.forecast.map((item, index) => (
+                <li key={index}>
+                    {item.Date} - {item.TemperatureC}°C - {item.Summary}
+                </li>
+                ))}
+            </ul>
+            </header>
+        </div>
+        );
+    }
+}
+
---
+++ my-k8s-app/frontend/src/types/WeatherForcastResource.ts
@@ -0,0 +1,6 @@
+﻿export interface WeatherForecastResource {
+    Date: string;
+    TemperatureC: number;
+    TemperatureF?:  number;
+    Summary?: string;
+}
